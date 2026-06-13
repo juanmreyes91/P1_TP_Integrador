96cdfac60525ee52ada85518d961cace9210027e
Carolina Rodríguez <carorodriguez027@gmail.com>
2026-06-13 13:39:25 +0200

    "Se renombró el archivo principal como main".

---
+++ main.py
@@ -0,0 +1,25 @@
+######################################################
+#######################  MAIN  #######################
+######################################################
+import Funciones as f
+
+#Definimos las estructuras de datos a utilizar
+lista = []
+diccionario = {}
+
+#Inicializamos el menú
+opcion = 0
+while opcion != 7:
+    opcion = f.menu()
+    if opcion == 1:
+        pass
+    elif opcion == 2:
+        pass
+    elif opcion == 3:
+        pass
+    elif opcion == 4:
+        pass
+    elif opcion == 5:
+        pass
+    elif opcion == 6:
+        pass    
